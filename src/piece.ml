7bd4920288cb68dd4f968ee2a6388478ce578523
Daniel Botros <dab463@cornell.edu>
2022-10-19 01:42:44 -0400

    Implemented more useful functions for Piece

--- src/piece.ml
+++ src/piece.ml
@@ -10,6 +10,15 @@ type piece_types =
   | Rook
   | Bishop
 
+type position = (char * int) option
+
+type piece = {
+  piece_type : piece_types;
+  position : position;
+  color : color;
+  first_move : bool;
+}
+
 let piece_to_string piece =
   match piece with
   | Pawn -> "pawn"
@@ -29,14 +38,7 @@ let string_to_piece str =
   | "bishop" -> Some Bishop
   | _ -> None
 
-type position = (char * int) option
-
-type piece = {
-  piece_type : piece_types;
-  position : position;
-  color : color;
-  first_move : bool;
-}
+let char_to_int c = Char.code c - Char.code '0'
 
 let create_piece p_type pos col =
   { piece_type = p_type; position = pos; color = col; first_move = true }
@@ -44,6 +46,7 @@ let create_piece p_type pos col =
 let get_piece piece = piece.piece_type
 let get_position piece = piece.position
 let get_color piece = piece.color
+let is_first_move piece = piece.first_move
 
 let same_pos piece1 piece2 =
   match (piece1.position, piece2.position) with
@@ -54,11 +57,26 @@ let capture_piece piece attacking_piece =
   if same_pos piece attacking_piece then { piece with position = None }
   else piece
 
-let valid_pos pos = match pos with
-| (c, i) -> Char.code 'a' <= Char.code c && Char.code c <= Char.code 'z' && i < 8 && i > 1
-| _ -> false
-let move_piece piece pos = { piece with position = Some pos; first_move = false }
+let valid_pos pos =
+  match pos with
+  | c, i ->
+      Char.code 'a' <= Char.code c
+      && Char.code c <= Char.code 'h'
+      && i < 8 && i > 1
+  | _ -> false
 
-let string_to_move str = match str with 
-| 
-|
+let move_piece piece pos =
+  { piece with position = Some pos; first_move = false }
+
+let pos_of_string str1 : position =
+  Some (str1.[0], int_of_char str1.[1] - int_of_char '0')
+
+let string_of_pos (pos : position) =
+  match pos with
+  | Some (c, i) -> Some (String.make 1 c ^ string_of_int i)
+  | None -> None
+
+let valid_pawn_move pos piece =
+  match piece.position, pos with
+  | Some (r1, c1), Some (r2, c2) -> ((r2 |> char_to_int) - (r1 |> char_to_int) |> abs) = 1 || (((r2 |> char_to_int) - (r1 |> char_to_int) |> abs)= 2 && piece.first_move = true))
+  | _ -> false
