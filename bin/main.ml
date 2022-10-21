dc749089bdd93a73aac20685a555e402c3b0d821
Young Zheng <yz486@cornell.edu>
2022-10-20 21:56:17 -0400

    Fixed main.ml to implement board methods - fixed play_game_helper and play_game

--- bin/main.ml
+++ bin/main.ml
@@ -21,36 +21,34 @@ let rec print_board = function
       print_endline (String.concat " " h);
       print_board t
 
-let rec play_game_helper new_board st =
-  print_board new_board;
+let rec play_game_helper st =
+  print_board (State.board st);
   print_endline
     "Enter 'move' followed by starting position and final position of the \
      desired move or 'quit' to exit:";
   match Command.parse (read_line ()) with
   | exception _ ->
       print_endline "This is not a valid move. Please try again: ";
-      play_game_helper new_board st
+      play_game_helper st
   | Go (x, y) ->
       print_endline "Valid move!";
-      play_game_helper
-        (Board.board_to_list State.board)
-        (State.move (x.[0], int_of_char x.[1]) st (y.[0], int_of_char y.[1]))
+      play_game_helper st
+        (**(State.move (x.[0], int_of_char x.[1]) st (y.[0], int_of_char y.[1]))*)
   | Quit ->
       print_endline "Game over. Hope you enjoyed playing!";
       exit 0
 
 (** [play_game new_board] starts the chess game. *)
 let play_game new_board =
-  print_endline "\n\nWelcome to your Game of Chess! ";
-  play_game_helper new_board (State.create_state (Board.init_board new_board))
+  play_game_helper (State.create_state (Board.init_board new_board))
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   print_endline "\n\nWelcome to chess!\n";
   print_endline "Please enter anything to begin your game: \n";
   print_string "> ";
-  match read_line () with
-  | _ -> play_game new_board
+  print_endline "\n\nWelcome to your Game of Chess! ";
+  play_game new_board
 
 (* Execute the game engine. *)
 let () = main ()
