891a469d90e854f4f24275d88ef6eb724dad3703
Natan Aklilu <nta6@cornell.edu>
2022-12-01 20:50:17 -0500

    Implement promotion and print graveyard on each side

--- bin/main.ml
+++ bin/main.ml
@@ -11,7 +11,7 @@ let new_board =
     [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
     [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
     [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+    [ "♟"; "♟"; "♟"; "♟"; "♟"; "♟"; "♟"; "♟" ];
     [ "♜"; "♞"; "♝"; "♛"; "♚"; "♝"; "♞"; "♜" ];
   ]
 
@@ -42,22 +42,54 @@ let coordinate_converter_col nmbr =
 let coordinate_converter (cmd : string) =
   coordinate_converter_col cmd.[1] ^ coordinate_converter_row cmd.[0]
 
-let print_board lst =
-  let rec helper c lst =
-    match lst with
-    | [] ->
+let print_board board grave =
+  let rec helper c bd gr =
+    match (bd, gr) with
+    | [], [ []; b_g ] ->
         print_endline " ";
-        print_endline "                    a b c d e f g h"
-    | h :: t ->
+        print_endline "                    a b c d e f g h     "
+    | [ h_b ], [ []; [] ] ->
         print_endline
-          ("                " ^ String.make 1 c ^ "   " ^ String.concat " " h);
-        helper (char_of_int (int_of_char c - 1)) t
+          ("                " ^ String.make 1 c ^ "   " ^ String.concat " " h_b);
+        helper (char_of_int (int_of_char c - 1)) [] [ []; [] ]
+    | [ h_b ], [ []; b_g ] ->
+        print_endline
+          ("                " ^ String.make 1 c ^ "   " ^ String.concat " " h_b
+         ^ "      Black Graveyard: " ^ String.concat " " b_g);
+        helper (char_of_int (int_of_char c - 1)) [] [ []; b_g ]
+    | h_b :: t_b, [ []; b_g ] ->
+        print_endline
+          ("                " ^ String.make 1 c ^ "   " ^ String.concat " " h_b);
+        helper (char_of_int (int_of_char c - 1)) t_b [ []; b_g ]
+    | h_b :: t_b, [ w_g; b_g ] ->
+        print_endline
+          ("                " ^ String.make 1 c ^ "   " ^ String.concat " " h_b
+         ^ "      White Graveyard: " ^ String.concat " " w_g);
+        helper (char_of_int (int_of_char c - 1)) t_b [ []; b_g ]
+    | _, _ -> failwith "Impossible"
   in
-  helper '8' lst
+  helper '8' board grave
+
+let rec grave_helper grave acc =
+  match (grave, acc) with
+  | [], acc -> acc
+  | "♟" :: t, [ w; b ] -> grave_helper t [ "♟" :: w; b ]
+  | "♞" :: t, [ w; b ] -> grave_helper t [ "♞" :: w; b ]
+  | "♚" :: t, [ w; b ] -> grave_helper t [ "♚" :: w; b ]
+  | "♛" :: t, [ w; b ] -> grave_helper t [ "♛" :: w; b ]
+  | "♜" :: t, [ w; b ] -> grave_helper t [ "♜" :: w; b ]
+  | "♝" :: t, [ w; b ] -> grave_helper t [ "♝" :: w; b ]
+  | "♙" :: t, [ w; b ] -> grave_helper t [ w; "♙" :: b ]
+  | "♘" :: t, [ w; b ] -> grave_helper t [ w; "♘" :: b ]
+  | "♔" :: t, [ w; b ] -> grave_helper t [ w; "♔" :: b ]
+  | "♕" :: t, [ w; b ] -> grave_helper t [ w; "♕" :: b ]
+  | "♖" :: t, [ w; b ] -> grave_helper t [ w; "♖" :: b ]
+  | "♗" :: t, [ w; b ] -> grave_helper t [ w; "♗" :: b ]
+  | _ -> failwith "Invalid call to grave_helper"
 
 let rec play_game_helper st =
   print_endline "";
-  print_board (State.board st);
+  print_board (State.board st) (grave_helper (State.graveyard st) [ []; [] ]);
   print_endline
     "\n\n\
      Enter 'move' followed by starting position and final position\n\
@@ -65,7 +97,7 @@ let rec play_game_helper st =
   match Command.parse (read_line ()) with
   | exception _ ->
       print_endline "";
-      print_endline "           This is not a valid move. Please try again: ";
+      print_endline "     This is not a valid move. Please try again: ";
       play_game_helper st
   | Move (x, y) -> (
       let x' = coordinate_converter x in
